9d22ac792706fcdc1d83efaf03edde414a236bb7
zyngjaku <zyngi98@gmail.com>
2018-12-11 22:44:26 +0100

    Daily commit (draft of login form)

---
+++ src/pl/edu/agh/student/zyngier/Controller.java
@@ -0,0 +1,4 @@
+package pl.edu.agh.student.zyngier;
+
+public class Controller {
+}
---
+++ src/pl/edu/agh/student/zyngier/Main.java
@@ -0,0 +1,82 @@
+package pl.edu.agh.student.zyngier;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static final String AIRLINE_NAME = "Example Airline";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle(AIRLINE_NAME + " Reservation System");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        System.out.println(primaryStage.getWidth());
+        System.out.println(primaryStage.getHeight());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+/*
+MENU
+
+BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 300, 250, Color.WHITE);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+        root.setTop(menuBar);
+
+        // File menu - new, save, exit
+        Menu fileMenu = new Menu("File");
+        MenuItem newMenuItem = new MenuItem("New");
+        MenuItem saveMenuItem = new MenuItem("Save");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+
+        fileMenu.getItems().addAll(newMenuItem, saveMenuItem,
+                new SeparatorMenuItem(), exitMenuItem);
+
+        Menu webMenu = new Menu("Web");
+        CheckMenuItem htmlMenuItem = new CheckMenuItem("HTML");
+        htmlMenuItem.setSelected(true);
+        webMenu.getItems().add(htmlMenuItem);
+
+        CheckMenuItem cssMenuItem = new CheckMenuItem("CSS");
+        cssMenuItem.setSelected(true);
+        webMenu.getItems().add(cssMenuItem);
+
+        Menu sqlMenu = new Menu("SQL");
+        ToggleGroup tGroup = new ToggleGroup();
+        RadioMenuItem mysqlItem = new RadioMenuItem("MySQL");
+        mysqlItem.setToggleGroup(tGroup);
+
+        RadioMenuItem oracleItem = new RadioMenuItem("Oracle");
+        oracleItem.setToggleGroup(tGroup);
+        oracleItem.setSelected(true);
+
+        sqlMenu.getItems().addAll(mysqlItem, oracleItem,
+                new SeparatorMenuItem());
+
+        Menu tutorialManeu = new Menu("Tutorial");
+        tutorialManeu.getItems().addAll(
+                new CheckMenuItem("Java"),
+                new CheckMenuItem("JavaFX"),
+                new CheckMenuItem("Swing"));
+
+        sqlMenu.getItems().add(tutorialManeu);
+
+        menuBar.getMenus().addAll(fileMenu, webMenu, sqlMenu);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+ */
